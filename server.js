3882a109745ce3a11fabb65b0dd41de771b72e35
Jason-Pearson <Jason-Pearson@users.noreply.github.com>
2017-02-03 22:52:58 -0500

    updated server(redirect), package(engines) Added index.html as well

--- server.js
+++ server.js
@@ -12,16 +12,23 @@ console.log(`Server listening at port: ${port}`); //need ` back quotes on tilde 
 //mounted two roots onto server
 
 // hello route - in proper order via mounting - '/(insert)' first before default '/'
+//this is an event listener (for hello root - request, reponse, nextRequest) passed into a Callback Function (Event Handler) via Anonymous-Arrow Function
+//app.use - for mounting middleware functions such as this
 app.use('/hello', (req, res, next) =>{
   //don't need set header with express
-    res.end("Hello FAM!")
+    //res.redirect("index.html") //the Response for this Callback is a res.redirect to a new html page
+      res.send("Hello...is it me you're looking for?")//res.send gives different html structure via Express
   next();
 });
 
 //anonymous Function with Arrow Function - indicate this root folder for the server to access - respond to the html page with "Welcome"
-//app.use same as app.get
+//app.use(Connect) same as app.get(Express, needs Post function as well however)
 // '/' main root for this website (webpage, can be hyerlinked?)
 app.use('/', (req, res, next) =>{
-    res.end("Welcome!")
+    res.send("Welcome!")//res.send gives different html structure via Express
+  
   next();
 });
+
+//takes this Express server file outside of the server
+module.exports = app; 
